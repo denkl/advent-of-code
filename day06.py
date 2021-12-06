204684eed8218c7618c719accbd421ec0bba1128
Denys Kolomytskyi <kolomytskyi@gmail.com>
2021-12-06 23:51:13 +0200

    Day 6, part 2

--- day06.py
+++ day06.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+from functools import lru_cache
+
+
 def read_input():
     with open("./inputs/day06") as f:
         return [int(i) for i in f.readline().strip().split(",")]
@@ -21,5 +24,34 @@ def part1():
     print(f"part 1: {len(ages)}")
 
 
+@lru_cache
+def descedants_num(timer, days):
+    num = 0
+    birthdays = grow(timer, days)
+    num += len(birthdays)
+    for day in birthdays:
+        num += descedants_num(8, day - 1)
+    return num
+
+
+@lru_cache
+def grow(timer, days_count):
+    birthdays = []
+    while days_count > 0:
+        if timer == 0:
+            timer = 6
+            birthdays.append(days_count)
+        else:
+            timer -= 1
+        days_count -= 1
+    return birthdays
+
+
+def part2():
+    num = sum((descedants_num(t, 256) + 1 for t in read_input()))
+    print(f"part 2: {num}")
+
+
 if __name__ == "__main__":
     part1()
+    part2()
