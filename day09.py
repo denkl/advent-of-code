1f71ab7a63e561e4aea3f079527b9eb25e2087ed
Denys Kolomytskyi <kolomytskyi@gmail.com>
2021-12-13 14:14:26 +0530

    Day 9, part 2

--- day09.py
+++ day09.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+from functools import reduce
+
+
 def read_input():
     heightmap = []
     with open("./inputs/day09") as f:
@@ -7,29 +10,68 @@ def read_input():
     return heightmap
 
 
-def part1():
-    heightmap = read_input()
+def get_adj(point, hw):
+    ri, ci = point
+    h, w = len(hw), len(hw[0])
+    adj = set()
+    if ci > 0:
+        adj.add((ri, ci - 1))
+    if ci < w - 1:
+        adj.add((ri, ci + 1))
+    if ri > 0:
+        adj.add((ri - 1, ci))
+    if ri < h - 1:
+        adj.add((ri + 1, ci))
+    return adj
+
+
+def risk_levels_points(heightmap):
     points = []
-    h, w = len(heightmap), len(heightmap[0])
 
     for ri, row in enumerate(heightmap):
         for ci, col in enumerate(row):
-            adj = []
-            if ci > 0:
-                adj.append(row[ci - 1])
-            if ci < w - 1:
-                adj.append(row[ci + 1])
-            if ri > 0:
-                adj.append(heightmap[ri - 1][ci])
-            if ri < h - 1:
-                adj.append(heightmap[ri + 1][ci])
+            adj = get_adj((ri, ci), heightmap)
+            if all([col < heightmap[ari][aci] for ari, aci in adj]):
+                points.append((ri, ci))
 
-            if all([col < i for i in adj]):
-                points.append(col)
+    return points
 
-    risk = sum([p + 1 for p in points])
+
+def part1():
+    heightmap = read_input()
+    points = risk_levels_points(heightmap)
+    risk = sum([heightmap[ri][ci] + 1 for ri, ci in points])
     print(f"part 1: {risk}")
+
+
+def part2():
+    heightmap = read_input()
+    low_points = risk_levels_points(heightmap)
+
+    seen = set()
+    basins = []
+    for point in low_points:
+        basin = set()
+        adj_locs = set()
+        adj_locs.add(point)
+
+        while adj_locs:
+            adj_p = adj_locs.pop()
+            seen.add(adj_p)
+            ri, ci = adj_p
+            if heightmap[ri][ci] == 9:
+                continue
+            basin.add(adj_p)
+            for i in get_adj(adj_p, heightmap):
+                if i not in seen:
+                    adj_locs.add(i)
+
+        basins.append(len(basin))
+
+    res = reduce(lambda x, y: x * y, sorted(basins, reverse=True)[:3])
+    print(f"part 2: {res}")
 
 
 if __name__ == "__main__":
     part1()
+    part2()
